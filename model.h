d0fbcbeb5cf4f8ae09c42c0df4ccd54b04d330fc
ruent <ed784@nyu.edu>
2020-07-30 23:03:43 -0400

    m

--- model.h
+++ model.h
@@ -4,7 +4,7 @@
 //CPLUS_INCLUDE_PATH =~/projects 
 //export CPLUS_INCLUDE_PATH
 #include <math.h> 
-#include "nonlinearsolver/methods.h"
+#include "nonlinsolver/methods.h"
 #include "marketdata/mdata.h"
 using namespace std;
 
@@ -42,12 +42,13 @@ struct lgmmeanreversion //int_0^t exp(-\int_0^s param du) ds
     }
 };
 
-struct basicswaption
+struct swaption
 {
     double swaptionExpiry; //in years = swap start time
     double swapTenor; //in years
     double strike;
     size_t numOfPeriods;
+    vector<unsigned short> paymTimes;
 }
 
 //should take a model (volatility and mean rversion param)
@@ -56,10 +57,10 @@ struct basicswaption
 //THIS WILL BE A FUNCTOR in volatility
 struct lgmswaptionprice
 {
-    basicswaption& s;
-    timezero& tZero; 
+    swaption& s;
+    timezero& z; 
     lgmonefactor& model; //model->meanrev
-    lgmswapprice swapprice;
+    lgmswapprice price;
 
 
     lgmswaptionprice() {}
@@ -69,24 +70,24 @@ struct lgmswaptionprice
         //there is composition here
         //so please see if it is better to write separate functions
         //and compose them, ala functional programming
-        double breakEvenRate = bisectionMethod(swapprice,-50,50,0.0000001);
+        double breakEvenRate = bisectionMethod(price,-50,50,0.0000001);
          
         double aux = 0;
         //auto e = discVector.begin(); e != discVector.end(); ++e
         for (size_t i = 0; i<s.numOfPeriods; ++i)
         {
-            double h = model.H(s.paymTimes[i]) - model.H(paymTimes[0]);
+            double h = model.H(s.paymTimes[i]) - model.H(s.paymTimes[0]);
             if (i == 0) 
-                aux += -discVector[i] * normalCdf(breakEvenRate/sqrt(vol));
+                aux += -z.discVector[i] * normalCdf(breakEvenRate/sqrt(vol));
             else
             {                
-                aux += tau[i]*discVector[i] * strike
+                aux += tau[i]*z.discVector[i] * s.strike
                         * normalCdf((breakEvenRate + vol* h)/sqrt(vol));
             }
             
-            if  (i == numOfPeriods-1)
+            if  (i == s.numOfPeriods-1)
             {
-                aux += discVector[i] 
+                aux += z.discVector[i] 
                         * normalCdf((breakEvenRate + vol* h)/sqrt(vol));
             } 
         }
@@ -106,8 +107,8 @@ class lgmpde //only explicit method based discrete version
 //using formula 5.7a from Hagan's "Evaluating and hedging ..."
 struct lgmswapprice
 {
-    lgmonefactor* model;  
-    timezero& tZero;
+    lgmonefactor& model;  
+    timezero& z;
     double vol;
     double fixrate;
     double swapstarttime;
@@ -118,9 +119,9 @@ struct lgmswapprice
         double value = 0.0;
         for (size_t i; i<n;++n)
         {
-            double hdiff = model->H(paymTimes[i]) - model->H(swapstarttime);
+            double hdiff = model.H(paymTimes[i]) - model.H(swapstarttime);
 
-            value += tau[i] * discVector[i]*exp(k())
+            value += tau[i] * z.discVector[i]*exp(k())
         }
 
         return value*fixrate;
